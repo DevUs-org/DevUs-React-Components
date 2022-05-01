538305904fe8967ea16f7d2ef3b51946fb1f09a0
Devjyot Singh Sidhu <devjyotsinhsidhu@gmail.com>
2022-05-01 16:45:26 +0530

    Added a better modal example

--- example/src/App.js
+++ example/src/App.js
@@ -10,10 +10,15 @@ const App = () => {
     modalIsOpen && document.querySelector('#modal-btn').addEventListener('click', () => setModalIsOpen(false));
   }, [modalIsOpen]);
 
+  function handleClick(e){
+    e.preventDefault();
+    setModalIsOpen(true);
+  }
+
   return (
     <div>
-      <button onClick={() => setModalIsOpen(true)}>Abc</button>
-      {modalIsOpen && <Modal type = 'error' message = 'This is a sample message ga ebbd hdbfbf hs hdbd dhe dhdbhd' isOpen={modalIsOpen}/>}
+      {modalIsOpen && <Modal type = 'alert' message = 'This is a sample message ga ebbd hdbfbf hs hdbd dhe dhdbhd' isOpen={modalIsOpen}/>}
+      <button type='submit' onClick={handleClick}>Abc</button>
     </div>
   );
 }
